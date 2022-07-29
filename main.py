978855e3bfd95ecce535977f1662d28491ec6f40
Dani <dani.93.perez@gmail.com>
2022-07-29 09:01:33 +0200

    nonDivisibleSubset in progress

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?isFullScreen=true
+#https://www.hackerrank.com/challenges/non-divisible-subset/problem?isFullScreen=true
 
 import math
 import os
@@ -7,18 +7,36 @@ import re
 import sys
 
 #
-# Complete the 'divisibleSumPairs' function below.
+# Complete the 'nonDivisibleSubset' function below.
 #
 # The function is expected to return an INTEGER.
 # The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER k
-#  3. INTEGER_ARRAY ar
+#  1. INTEGER k
+#  2. INTEGER_ARRAY s
 #
 
-def test(n, k, ar):
-    return 'test code here before moving it to a folder'
+
+
+def nonDivisibleSubset(k, s):
+    max_subset = 0
+    for i in range(len(s)-1):
+        divisible_count = s[i] % k
+        count = 0
+        for j in range(i+1, len(s)):
+            if (divisible_count + s[j] % k) % k:
+                divisible_count += s[j] % k
+                count += 1
+        if divisible_count % k != 0:
+            if count > max_subset:
+                max_subset = count
+
+    print(max_subset)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(test(6, 3, [1, 3, 2, 6, 1, 2]))
+    print(4%3)
+
+    print(nonDivisibleSubset(4, [19,10,12,24,25,22]))
+
+# 278,576,496,727,410,124,338,149,209,702,282,718,771,575,436
